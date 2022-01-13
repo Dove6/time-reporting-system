3330a683e75143073e3485af00dd58292fc93c14
Dove6 <dawidsygocki1234@wp.pl>
2022-01-13 10:21:47 +0100

    Show monthly project/time summary

--- lab4/ClientApp/src/reports/MonthlyReport.tsx
+++ lab4/ClientApp/src/reports/MonthlyReport.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {LastDateContext} from "../App";
+import MonthlyReportModel from "../models/MonthlyReport";
+import fetchData from "../fetchData";
+import {Table} from "react-bootstrap";
 
 export default function MonthlyReport() {
-    return (
-        <div>
-            <h1>MonthlyReport</h1>
-            <p>This is a simple example of a React component.</p>
-        </div>
-    );
+    const lastDateState = useContext(LastDateContext);
+    const getLastMonth = () => lastDateState.state.lastDate.substring(0, 7);
+    const setLastMonth = (monthString: string) => lastDateState.setLastDate(`${monthString}-01`);
+
+    const [monthlyReport, setMonthlyReport] = useState<MonthlyReportModel | null>(null);
+    const refreshDailyReport = () => {
+        fetchData(`/api/reports/${getLastMonth()}`)
+            .then(data => setMonthlyReport(data));
+    }
+    useEffect(refreshDailyReport, [lastDateState.state.lastDate]);
+
+    return (<>
+        <h1>Raport czasu pracy na miesiąc {getLastMonth()}</h1>
+        <Table>
+            <thead>
+            <tr>
+                <th>Projekt</th>
+                <th>Zadeklarowany czas</th>
+                <th>Zaakceptowany czas</th>
+            </tr>
+            </thead>
+            <tbody>
+            {monthlyReport?.projectTimeSummaries.map(e => (<tr key={e.projectCode}>
+                <td>{e.projectCode}</td>
+                <td>{e.time}</td>
+                <td>{e.acceptedTime}</td>
+            </tr>))}
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>Razem</th>
+                <th>{monthlyReport?.projectTimeSummaries.reduce((acc, val) => +acc + +val.time, 0)}</th>
+                <th>{monthlyReport?.projectTimeSummaries.reduce((acc, val) => +acc + +(val.acceptedTime ?? 0), 0)}</th>
+            </tr>
+            </tfoot>
+        </Table>
+    </>);
 }
