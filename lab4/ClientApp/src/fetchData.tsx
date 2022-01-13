84d98f6c2683c1dd00a44167cc5ef793454c006b
Dove6 <dawidsygocki1234@wp.pl>
2022-01-13 11:27:15 +0100

    Parse application/problem+json responses

--- lab4/ClientApp/src/fetchData.tsx
+++ lab4/ClientApp/src/fetchData.tsx
@@ -11,10 +11,18 @@ export default function fetchData(url: string, method: string = 'GET', payload?:
     return fetch(url, config)
         .then(async response => {
             // based on: https://jasonwatmore.com/post/2021/09/22/fetch-vanilla-js-check-if-http-response-is-json-in-javascript
-            const isJson = response.headers.get('content-type')?.includes('application/json');
+            const isJson = response.headers.get('content-type')?.includes('application/json') ||
+                response.headers.get('content-type')?.includes('application/problem+json');
             const data = isJson ? await response.json() : null;
-            if (!response.ok)
-                return Promise.reject((data && data.message) || response.statusText);
+            if (!response.ok) {
+                console.log(data);
+                let returnData = response.statusText;
+                if (data && data.message)
+                    returnData = data.message;
+                if (data && data.errors && Object.entries(data.errors)[0])
+                    returnData = `${Object.entries(data.errors)[0][0]}: ${Object.entries(data.errors)[0][1]}`
+                return Promise.reject(returnData);
+            }
             return Promise.resolve(data);
         });
 }
